55d3aa4cebee1576fe8aef0670e57a2fe15edcf9
Martin Kasak <MartinKasak@users.noreply.github.com>
2016-10-11 14:07:38 +0300

    teine kodutoo

--- data.php
+++ data.php
@@ -36,13 +36,13 @@
 		!empty($_POST["color"])
 	  ) {
 		  
-		saveCar(cleanInput($_POST["plate"]), $_POST["color"]);
+		saveWorkout(cleanInput($_POST["paev"]), $_POST["harjutus"], $_POST["harjutus"]);
 		
 	}
 	//saan kõik auto andmed
-	$carData = getAllCars();
+	$workoutData = getAllWorkout();
 	echo"<pre>";
-	var_dump($carData);
+//	var_dump($workoutData);
 	echo"</pre>";
 	
 	
@@ -53,8 +53,9 @@
 
 <p>Tere tulemast <?=$_SESSION["userEmail"];?> ! <a href= "?logout=1">Logi välja</a> </p>
 <form method="POST">
-<h2>Salvesta auto nr</h2><input type="text" name="plate" >
-<h2>Salvesta auto värv</h2> <input type="color" name="color" >
+<h2>Salvesta harjutuse paev</h2><input type="text" name="paev" >
+<h2>Salvesta harjutuse nimi</h2> <input type="text" name="harjutus" >
+<h2>Sisesta minutid</h2> <input type="text" name="minutid" >
 <br><br>
 <input type="submit" value="Salvesta andmed">
 
@@ -69,18 +70,21 @@
 	
 	$html .= "<tr>";
 		$html .= "<th>id</th>";
-		$html .= "<th>plate</th>";
-		$html .= "<th>color</th>";
+		$html .= "<th>paev</th>";
+		$html .= "<th>harjutus</th>";
+		$html .= "<th>minutid</th>";
 	$html .= "</tr>";
 	
 	//iga liikme kohta massiivis
-	foreach($carData as $c){
+	foreach($workoutData as $w){
 		//iga auto on $cal_days_in_month
 		//echo $c->plate."<br>";
 		$html .= "<tr>";
-			$html .= "<td>".$c->id."</td>";
-			$html .= "<td>".$c->plate."</td>";
-			$html .= "<td style ='background-color:".$c->carColor."'>".$c->carColor."</td>";
+			$html .= "<td>".$w->id."</td>";
+			$html .= "<td>".$w->paev."</td>";
+			$html .= "<td>".$w->harjutus."</td>";
+			$html .= "<td>".$w->minutid."</td>";
+			//$html .= "<td style ='background-color:".$c->carColor."'>".$c->carColor."</td>";
 		$html .= "</tr>";
 		
 	}
@@ -90,13 +94,13 @@
 	
 	$listhtml = "<br><br>";
 	
-	foreach($carData as $c){
+	/*foreach($carData as $c){
 		
 		$listhtml .= "<h1 style='color:".$c->carColor."'>".$c->plate."</h1>";
 		$listhtml .="<p>color = ".$c->carColor."</p>";
 		
 	}
-	echo $listhtml;
+	echo $listhtml;*/
 	
 	
 	
